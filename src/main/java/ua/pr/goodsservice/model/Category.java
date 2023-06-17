a0ec760f2b64e362c5a41ed8ab3fcd08dcac3c47
Nadiia Rubantseva <nadyafranko@gmail.com>
2023-06-17 22:38:41 +0300

    add category and product models

---
+++ src/main/java/ua/pr/goodsservice/model/Category.java
@@ -0,0 +1,26 @@
+package ua.pr.goodsservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name="categories")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "category", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private List<Product> products;
+
+}
---
+++ src/main/java/ua/pr/goodsservice/model/Product.java
@@ -0,0 +1,27 @@
+package ua.pr.goodsservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private long price;
+
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(name="category_id")
+    private Category category;
+
+}
